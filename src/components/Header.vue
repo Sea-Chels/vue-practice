5b2e5fb6be38cb42244a3a15bab3697cabfb9ddf
MissingNewt <chelby.sallady@gmail.com>
2023-10-24 11:46:19 -0500

    pushing halfway point

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonItem text="Add Task" color="green"/>
+    </header>
+ </template>
+ <script>
+ import ButtonItem from './Button.vue'
+    export default {
+        name: 'HeaderItem',
+        // props: ['title'] this is a simple version of defining props with no type checks. 
+        props: {
+            title: {
+                type: String,
+                default: 'Hello World' // you can leave it as just props: { title: String } if you want but you can also put in more prop details 
+            }
+        },
+        components: {
+            ButtonItem,
+        }
+    }
+</script>
+<style scoped>
+header {
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <TaskItem @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task"/>
+    </div>
+</template>
+<script>
+import TaskItem from './Task.vue'
+export default {
+    name: 'TaskContainer',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        TaskItem,
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
